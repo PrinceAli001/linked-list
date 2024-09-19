b2ad82d2cd602bf43e90fbe54415ec733524ef13
Mahfouz <mafz060706@gmail.com>
2024-09-19 17:03:24 +0100

    Project initialization

---
+++ index.js
@@ -0,0 +1,14 @@
+class linkedlist{
+    list = {};
+
+    append(value){
+        this.list += value;
+    }
+    prepend(value){
+        
+    }
+};
+class node{
+    value = null;
+    nextNode = null;
+}
