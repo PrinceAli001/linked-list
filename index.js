e741b9ba52093c2ac4dd4c0aeaa4649c287cefa5
Mahfouz <mafz060706@gmail.com>
2024-09-20 17:41:28 +0100

    Adding the methods of the linked list

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 class linkedlist{
     list = {};
-
+    
     append(value){
-        this.list += value;
-    }
+        let mutatedList = {...this.list,value};
+        this.list = mutatedList;
+        return this.list;
+    };
     prepend(value){
-        
-    }
+        let mutatedList = {value,...this.list};
+        this.list = mutatedList;
+        return this.list;
+    };
+    size(){
+        return this.list.length;
+    };
+    head(){
+        let head = {...this.list[0]};
+        return head;
+    };
+    tail(){
+        let tail = {...this.list[this.list.length-1]};
+        return tail;
+    };
+    at(index){
+        let mutatedList = {...this.list[index]};
+        return mutatedList;
+    };
+    pop(){
+        let mutatedList = {...this.list}.pop();
+        return mutatedList;
+    };
+    contains(value){
+        let mutatedList = {...this.list};
+        mutatedList.forEach(element => {
+            return (element === value) ? true : false;
+        });
+    };
+    find(value){
+        let mutatedList = {...this.list};
+        mutatedList.forEach(element => {
+            return (element === value) ? mutatedList.indexOf(element) : null;
+        });
+    };
 };
 class node{
     value = null;
